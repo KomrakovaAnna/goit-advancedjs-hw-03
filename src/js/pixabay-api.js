29e6725418d95364a24d1809e77287fee2a1b1a0
Anna Komrakova <komrakova.anna@gmail.com>
2024-08-04 19:43:49 +0100

    redeploy

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,6 +1,6 @@
 const API_KEY = '45172524-3969727cd49dc0a3e64f2f5a7';
 
-export const apiCall = (searchValue) => {
+export const apiCall = searchValue => {
   const searchParams = new URLSearchParams({
     key: API_KEY,
     q: searchValue,
@@ -9,16 +9,14 @@ export const apiCall = (searchValue) => {
     safesearch: 'true',
   });
   return fetch(`https://pixabay.com/api/?${searchParams}`)
-  .then(response => {
-    if (!response.ok) {
-      throw new Error(response.status);
-    }
-    return response.json();
-  })
-  .catch(error => {
-    console.error(error);
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error(error);
       throw error;
-    
-  });
+    });
 };
-
